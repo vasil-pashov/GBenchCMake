bcda3f708c65506612bbf6a2f05f924d459784cf
Vasil Pashov <vasil.pashov1@gmail.com>
2020-12-18 17:55:56 +0200

    Fix the new format for drawing plots, based on google benchmark fixtures

--- DrawGraphs/plot.py
+++ DrawGraphs/plot.py
@@ -7,8 +7,9 @@ def PlotColumnException(Exception):
     self.message=message
 
 
-def makePlotColumnDesc(id: str, label: str=id, type: str="string", role: typing.Union[str, None]=None):
+def makePlotColumnDesc(id: str, label: str=None, type: str="string", role: typing.Union[str, None]=None):
   """Create description for a column in DataTable
+
   :param id: Id of the column used to reference it internaly
   :type id: str
   :param label: Labe of the column, this will be shown to the user
@@ -18,13 +19,14 @@ def makePlotColumnDesc(id: str, label: str=id, type: str="string", role: typing.
   :param role: Role of the column (annotation, tooltip, etc.)
   :type role: str
   """
+
   allowedTypes=["string", "date", "datetime", "number", "boolean", "timeofday"]
   if type not in allowedTypes:
     raise PlotColumnException(
       "Type {} is not supported. supported types are: {}".format(type, allowedTypes))
   colDesc={
     'id': id,
-    'label': label,
+    'label': label if label else id,
     'type': type
   }
   if role:
@@ -38,9 +40,11 @@ class PlotDescriptionException(Exception):
 
 class PlotDescription:
   """Container for all columns descriptions and the domain variable id
+
   domainId: id for the column which will be the domain value (x-axis) 
   columns: Dict which will hold the columns of the DataTable
   """
+
   def __init__(self, domainId: str, columnList: list):
     """Initialize the plot.
 
@@ -52,6 +56,7 @@ class PlotDescription:
     The domain column must be the first in this list
     :type columnList: list
     """ 
+
     self.domainId=domainId
     self.columns=OrderedDict((col['id'], col) for col in columnList)
     if domainId not in self.columns:
@@ -59,46 +64,21 @@ class PlotDescription:
 
   def addColumn(self, column):
     """Add column to the column. Keeps ordering.
+
     :param column: The column which will be added
     """
-    self.columns[column.id]=column
+
+    self.columns[column['id']]=column
 
   def containsColumn(self, column):
     return column['id'] in self.columns
 
 class PlotRowException(Exception):
-  def __init__(self, domainId, message):
+  def __init__(self, message):
     self.message=message
 
-def _toJsonStrValue(type: str, value) -> str:
-  """Return string representation of value which could be accepted by JSON.parse
-  :param type: Name of the type of value
-  :type type: str
-  :param value: The value which will be reformated as acceptable string value
-  """
-  if value is None:
-    return "null"
-  if type == "datetime":
-    strVal="\"Date(%d, %d, %d, %d, %d, %d)\"" % (
-      value.year,
-      value.month - 1,
-      value.day,
-      value.hour,
-      value.minute,
-      value.second
-    )
-  elif type == "date":
-    strVal="\"Date(%d, %d, %d)\"" % (
-      value.year,
-      value.month - 1,
-      value.day
-    )
-  else:
-    strVal=str(value)
-  return strVal
 
-
-class Plot:
+class DataTable:
   """Holds data for a plot in format from whic is easy to create google chart
 
   __description: Description of each variable which is going to be plotted
@@ -107,6 +87,7 @@ class Plot:
   _options: Key value pairs with additional options. Will be used as-is in the resulting
   html page with the charts. Can contain any valid google chart options
   """
+
   def __init__(self, description):
     self.__description=description
     self.__rows=[]
@@ -121,8 +102,58 @@ class Plot:
     self._options=options
 
   @property
-  def rows(self):
+  def domainId(self):
+    return self.__description.domainId  
+
+  @property
+  def columns(self):
+    return self.__description.columns.keys()
+
+  def __iter__(self):
     return iter(self.__rows)
+
+  def __getitem__(self, rowId):
+    if rowId < 0 or rowId >= len(self.__rows):
+      raise PlotRowException(
+        "Trying to access row with invalid id: {}".format(rowId))
+    return self.__rows[rowId]
+
+  def __setitem__(self, rowId, values):
+    if self.__description.domainId not in values:
+      raise PlotRowException("Row is missing domain value.")
+    if rowId < 0 or rowId >= len(self.__rows):
+      raise PlotRowException(
+        "Trying to access row with invalid id: {}".format(rowId))
+    self.__rows[rowId]=values
+
+  def _toJsonStrValue(self, colId: str, value) -> str:
+    """Return string representation of value which could be accepted by JSON.parse
+  
+    :param colId: Name of the column
+    :type colId: str
+    :param value: The value which will be reformated as acceptable string value
+    """
+    type=self.__description.columns[colId]['type']
+    if value is None:
+      return "null"
+    if type == "datetime":
+      strVal="\"Date(%d, %d, %d, %d, %d, %d)\"" % (
+        value.year,
+        value.month - 1,
+        value.day,
+        value.hour,
+        value.minute,
+        value.second
+      )
+    elif type == "date":
+      strVal="\"Date(%d, %d, %d)\"" % (
+        value.year,
+        value.month - 1,
+        value.day
+      )
+    else:
+      strVal=str(value)
+    return strVal
 
   def optionsJSON(self):
     return json.dumps(self._options)
@@ -131,29 +162,24 @@ class Plot:
     if self.__description.containsColumn(colDesc) == False:
       self.__description.addColumn(colDesc)
 
-  def addRow(self, rowData: dict):
+  def addRow(self, rowData: dict) -> int:
     """Adds domain value and the values at other variables at this domain value
+
     :param rowData: Key/value store with the values of the domain and the other variables
     the keys must be the id-s of the columns (ordering here is not important). The domain
     variable must be in the rowData. Other variables are optional. ID-s which are not in
     the description are not allowed.
+    :returns: Unique identifier for the added row
     """
+
     if self.__description.domainId not in rowData:
       raise PlotRowException("Row is missing domain value.")
-    diff=rowData.keys() - self.__description.columns.keys()
+    diff=rowData.keys() - self.columns
     if len(diff):
       raise PlotRowException("Adding row with unknown columns: {}".format(diff))
+    idx=len(self.__rows)
     self.__rows.append(rowData)
-
-  def __getRowDescStr(self, row: dict) -> str:
-    """Create string with the sythax of JavaScript array for a row of the plot
-
-    Will use the same ordering as in the colums of the description.
-    The domainId will be first and so on.
-    :param row: The row which will be converted to string acceptable by JSON.parse
-    """
-    getVal=lambda item: _toJsonStrValue(item[1]['type'], row.get(item[0], None))
-    return "[%s]" % (",".join(map(getVal, self.__description.columns.items())))
+    return idx
 
   def toGoogleChartArrayStr(self):
     """Create JSON representation for the plot
@@ -167,5 +193,59 @@ class Plot:
     ]
     """
     cols=[colDesc for colDesc in self.__description.columns.values()]
-    rowDescrptionString=",".join(map(self.__getRowDescStr, self.__rows))
-    return "\'[%s, %s]\'" % (json.dumps(cols), rowDescrptionString)
+    createRow = lambda row: "[%s]" % (",".join([self._toJsonStrValue(col, row[col]) for col in self.columns]))
+    rowsString=",".join([createRow(row) for row in self.__rows])
+    return "\'[%s, %s]\'" % (json.dumps(cols), rowsString)
+
+
+class Plot(DataTable):
+  """Class to represent a function plot.
+
+  The domain column represents the x-axis. All other columns represent
+  different functions, all of which are going to be plotted on the same graph
+  __xAxisIndex: Key-value store where the keys are values of the domain column
+  and the values are indexes into __rows array.
+  """
+
+  def __init__(self, description):
+    super().__init__(description)
+    self.__xAxisIndex={}
+
+  def addValue(self, domainValue, values):
+    """Adds values for different graphs for the given domainValue
+
+    :param domainValue: x-axis value
+    :param values: Key-value store where keys are names of plots
+    and values are value of the plots at the given domainValue
+    """
+    for col in values:
+      if col not in self.columns:
+        raise PlotRowException("Adding value to an unknown column: {}".format(col))
+    idx=self.__xAxisIndex.get(domainValue, -1)
+    if idx == -1:
+      row={**values, self.domainId: domainValue}
+      rowId=self.addRow(row)
+      self.__xAxisIndex[domainValue]=rowId
+    else:
+      super().__getitem__(idx).update(values)
+
+  def __setitem__(self, domainValue, values):
+    idx=self.__xAxisIndex.get(domainValue, -1)
+    if idx == -1:
+      row={**values, self.domainId: domainValue}
+      rowId=self.addRow(row)
+      self.__xAxisIndex[domainValue]=rowId
+    else:
+      super().__setitem__(idx, values)
+
+  def __getitem__(self, domainValue):
+    idx=self.__xAxisIndex[domainValue]
+    return super().__getitem__(idx)
+
+  def __iter__(self):
+    """Iterate over all domainValues.
+
+    The values are not guaranteed to have a specific order
+    """
+
+    return iter(self.__xAxisIndex)
